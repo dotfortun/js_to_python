a10c138b6fd3a52a627ec4aa254ee78d19c06a0c
dotfortun <shane.bell.dev@gmail.com>
2022-06-29 23:38:49 +0000

    I've held you here for too long.

--- main.py
+++ main.py
@@ -1,282 +1,288 @@
-# Our code is going to go in here.
+import math
+from fractions import Fraction
+from pprint import pprint
 
-# # This file has been linked to the html file.
+# Data Types:
+some_var = "This is a variable now!"
 
-# function listLonger() {
-#   let newLi = document.createElement('li');
-#   newLi.innerHTML = "This was made in JS.";
+# print(some_var)
 
-#   document
-#     .querySelector('ul')
-#     .appendChild(newLi);
-# }
+some_string = "This is a string."
+some_string_halfquote = 'This is a string.'
+some_string_escaped = "This is a string with \"quotes in it\"."
+some_string_multiline = """
+This is a string.  It can have multiple lines in it, and even matching quotes like so: " " " " " ".
+"""
+some_string_multiline_half = '''
+This is also a multiline string.
+'''
+some_fmt_string = "Some math numbers: {pi}, {tau}, {e}"
+pyfmt_url = "https://pyformat.info/"
+
+
+# print(some_fmt_string.format(
+#     pi=math.pi,
+#     e=math.e,
+#     tau=math.tau
+# ))
+
+some_float = 0.0
+some_int = 0
+# print(0.1 + 0.2)
+# print(Fraction(1, 10) + Fraction(2, 10))
+
+some_true = True
+some_false = False
+
+some_dict = {
+    "some key": "some value",
+    "pi": math.pi,
+    "e": math.e,
+    "tau": math.tau
+}
+
+# pprint(some_dict)
+# print(some_dict["pi"])
+
+some_list = [
+    "They contain a bunch of items.",
+    "of any data type,",
+    "including other lists:",
+    ["Like this!"],
+    "https://docs.python.org/3/library/stdtypes.html#sequence-types-list-tuple-range"
+]
+
+# pprint(some_list)
+# some_list.append("This value was added after the list was created!")
+# pprint(some_list)
+
+some_none = None
+
+#---------------------------------------------------------------#
 
 # # This array represents a cyberpunk library.
-# books = [
-#     # This object represents a book.
-#     {
-#       title: "Neuromancer",
-#       author: "William Gibson",
-#       year_published: 1984,
-#       isbn: "",
-#       rating: 100
-#     },
-#     {
-#       title: "Snow Crash",
-#       author: "Neal Stephenson",
-#       year_published: 1992,
-#       isbn: "978-01336162-0",
-#       rating: 100
-#     },
-#     {
-#       title: "Altered Carbon",
-#       author: "Richard K. Morgan",
-#       year_published: 2002,
-#       isbn: "",
-#       rating: 100
-#     }
-#   ];
-  
-#   # This is an arrow function.
-#   const bookFormatter = (book) => {
-#     return `Title: ${book.title}, Author: ${book.author}`;
-#   }
-  
-#   # This is just a plain function.
-#   function bookYearFormatter(book) {
-#     return `Year: ${book.year_published}`;
-#   }
-  
-#   # --------------------------------------
-  
-#   # This is a for of loop.
-#   for (book of books) {
-#     # Javascript string formatting.
-#     # console.log(bookFormatter(book));
-#     # console.log(bookYearFormatter(book));
-  
-#     # String formatting
-#     console.log(`
-#       <div>
-#         <h1>${book.title}</h1>
-#         <h2>${book.author}</h2>
-#       </div>
-#     `);
-  
-#     # String concatenation
-#     console.log(
-#       '<div><h1>'
-#         + book.title
-#         + '</h1><h2>'
-#         + book.author
-#         + '</h2></div>'
-#     );
-#   }
-  
-#   # let string_a = "This is the beggining of the ";
-#   # let string_b = "sentence that this is the end of."
-  
-#   # console.log(string_a);
-#   # console.log(string_b);
-#   # console.log(string_a + string_b);
+books = [
+    {
+        "title": "Neuromancer",
+        "author": "William Gibson",
+        "year_published": 1984,
+        "isbn": "",
+        "rating": 100
+    },
+    {
+        "title": "Snow Crash",
+        "author": "Neal Stephenson",
+        "year_published": 1992,
+        "isbn": "978-01336162-0",
+        "rating": 100
+    },
+    {
+        "title": "Altered Carbon",
+        "author": "Richard K. Morgan",
+        "year_published": 2002,
+        "isbn": "",
+        "rating": 100
+    }
+]
 
+# for book in books:
+#     pprint(book)
+#     print("----")
+# print("This is out of the scope of the loop.")
 
-# window.onload = function() {
-#   # let today = "This was updated by script.js.";
-#   # document.querySelector('p').innerHTML = today;
+# for idx, book in enumerate(books):
+#     print(idx, book)
 
-#   let someString = "This is a string.";
-#   # document.querySelector('p').innerHTML = someString;
+# for x in range(1, 11):
+#     print(x)
 
-#   let someNumber = 1337;
-#   # document.querySelector('p').innerHTML = someNumber;
+# while len(books) < 5:
+#     print("We're in a loop!")
+#     books.append("")
 
-#   let someBoolean = false;
-#   # document.querySelector('p').innerHTML = (
-#   #   1337 === "1337"
-#   # );
+# print("What do you think is going to happen when I run other.py?")
 
-#   #              | 0    1    2    3    4 |
-#   let someArray = ["a", "b", "c", "d", "e"];
-#   # document.querySelector('p').innerHTML = someArray[0];
-#   # document.querySelector('li').innerHTML = someArray[4];
+# def some_function():
+#     print("This is in the function!")
+#     return "This is the return value."
 
-#   let someObject = {
-#     someKey: "some value",
-#     anotherKey: false,
-#     aThirdKey: 9001,
-#     noKey: null
-#   };
+# print(some_function.__name__)
+# print(some_function())
 
-#   # document.querySelector('p').innerHTML = someObject.anotherKey;
+# some_lambda = lambda x, y: x + y
 
-#   let someUndefined;
-#   # document.querySelector('p').innerHTML = someUndefined;
+# print(some_lambda.__name__)
+# print(some_lambda(0.1, 0.2))
 
-#   let someNull = null;
-#   # document.querySelector('p').innerHTML = someNull;
+# print("One string " + "another string.")
+# print(" ".join(["This", 'list', "will", 'be', 'joined', 'with', 'spaces.']))
 
-#   document
-#     .getElementById("listLonger").onclick = listLonger;
-# }
+# if __name__ == "__main__":
+#     print("This will only run if the file is run manually.")
+
+# print(__name__)
 
 # This array represents a cyberpunk library.
-# books = [
-#   # This object represents a book.
-#   {
-#     title: "Neuromancer",
-#     author: "William Gibson",
-#     year_published: 1984,
-#     isbn: "",
-#     rating: 90,
-#     is_checked_out: false,
-#   },
-#   {
-#     title: "Snow Crash",
-#     author: "Neal Stephenson",
-#     year_published: 1992,
-#     isbn: "978-01336162-0",
-#     rating: 95,
-#     is_checked_out: true,
-#   },
-#   {
-#     title: "Altered Carbon",
-#     author: "Richard K. Morgan",
-#     year_published: 2002,
-#     isbn: "",
-#     rating: 100,
-#     is_checked_out: false,
-#   },
-#   {
-#     title: "Cryptonomicon",
-#     author: "Neal Stephenson",
-#     year_published: 1999,
-#     isbn: "978-0-380-78862-0",
-#     rating: 85,
-#     is_checked_out: false,
-#   }
-# ];
+books = [
+  # This object represents a book.
+  {
+    "title": "Neuromancer",
+    "author": "William Gibson",
+    "year_published": 1984,
+    "isbn": "",
+    "rating": 90,
+  },
+  {
+    "title": "Snow Crash",
+    "author": "Neal Stephenson",
+    "year_published": 1992,
+    "isbn": "978-01336162-0",
+    "rating": 95,
+  },
+  {
+    "title": "Altered Carbon",
+    "author": "Richard K. Morgan",
+    "year_published": 2002,
+    "isbn": "",
+    "rating": 100,
+  },
+  {
+    "title": "Cryptonomicon",
+    "author": "Neal Stephenson",
+    "year_published": 1999,
+    "isbn": "978-0-380-78862-0",
+    "rating": 85,
+  }
+]
 
-# let lipsum = `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+# pprint(
+#     list(
+#         map(
+#             lambda x: x["title"] + ", " + x["author"],
+#             books
+#         )
+#     )
+# )
+
+# pprint(
+#     list(
+#         filter(
+#             lambda x: x["year_published"] > 1992,
+#             books
+#         )
+#     )
+# )
+
+# print("Unsorted:")
+# pprint(books)
+# books.sort(key=lambda x: x["rating"])
+# print("books.sort():")
+# pprint(books)
+# print("sorted(books):")
+# pprint(sorted(books, key=lambda x: x["year_published"]))
+
+# lipsum = '''Lorem ipsum dolor sit amet, consectetur adipiscing elit.
 # Etiam consequat dapibus mauris ut accumsan.
 # Integer suscipit metus justo, eget pharetra urna eleifend ut.
 # Nam eu ultrices diam. Aenean bibendum tortor sed ligula vestibulum, eu iaculis neque euismod.
 # Vivamus aliquet mauris sit amet tortor scelerisque cursus.
 # Nunc pretium feugiat posuere.
 # Curabitur congue ligula quis luctus laoreet.
-# Praesent suscipit tempor ante nec fringilla.`;
+# Praesent suscipit tempor ante nec fringilla.''';
 
-# # String splitting
-# # for (word of lipsum.split("\n")) {
-# #   console.log(word.toUpperCase());
-# # }
+# print(lipsum.isascii())
 
-# # For loop on an array.
-# # for (let iterator = 0; iterator < books.length; iterator++) {
-# #   console.log(iterator, books[iterator]);
-# # }
+class Book(object):
+    title = None
+    year_published = None
+    isbn = None
+    author = None
+    rating = None
 
-# # For loop on a string.
-# # for (let i = 0; i < lipsum.length; i++) {
-# #   console.log(lipsum[i]);
-# # }
+    def __init__(
+        self,
+        title=None,
+        year_published=None,
+        isbn=None,
+        author=None,
+        rating=None,
+    ):
+        self.title = title
+        self.year_published = year_published
+        self.isbn = isbn
+        self.author = author
+        self.rating = rating
 
-# # for ... in
-# # for (idx in books) {
-# #   console.log(idx);
-# #   console.log(books[idx]);
-# # }
+    def __repr__(self):
+        return "<Book {title} by {author}>".format(
+            title=self.title,
+            author=self.author
+        )
 
-# # for ... of
-# # for (book of books) {
-# #   console.log(book);
-# # }
+    def __eq__(self, other):
+        return self.__dict__ == other.__dict__
 
-# # For Each
-# # books.forEach(book => {
-# #   console.log(book);
-# # });
+    def serialize(self):
+        return {
+            "title": self.title,
+            "author": self.author,
+            "year_published": self.year_published,
+            "isbn": self.isbn,
+            "rating": self.rating,
+        }
+    
+    @staticmethod
+    def deserialize(x):
+        return Book(
+            title=x.get("title", None),
+            year_published=x.get("year_published", None),
+            isbn=x.get("isbn", None),
+            author=x.get("author", None),
+            rating=x.get("rating", None)
+        )
+    
 
-# # Map
-# # console.log(books);
-# # let new_books = books.map(book => {
-# #   let new_book = book;
-# #   book.is_checked_out = true;
-# #   return new_book;
-# # });
-# # console.log(new_books);
+books_obj = list(
+    map(
+        lambda x: Book.deserialize(x),
+        books
+    )
+)
 
-# # let new_books = books;
-# # console.log(books);
-# # for (idx in new_books) {
-# #   new_books[idx].is_checked_out = true;
-# # }
-# # console.log(new_books);
+# pprint(books)
+# pprint(books_obj)
 
-# # let i = 1;
-# # while (i < 2**16) {
-# #   console.log(i);
-# #   i = i*2;
-# # }
+# for book in books_obj:
+#     pprint(book.serialize())
 
-# # Filter
-# # let booksInLibrary = books.filter(book => {
-# #   return !book.is_checked_out;
-# # });
+# neuro = Book.deserialize(
+#     books_obj[0].serialize()
+# )
 
-# # function sortingFunction(a, b) {
-# #   return a.rating - b.rating;
-# # }
+# neuro_copy = Book.deserialize(
+#     books_obj[0].serialize()
+# )
 
-# # Sort
-# # books.sort((a, b) => {
-# #   # a > b if a - b > 0
-# #   # a < b if a - b < 0
-# #   # a = b if a - b = 0
-# #   # return b.year_published - a.year_published;
+# print("Equality:", neuro_copy == neuro)
+# print("Identity:", neuro_copy is neuro)
 
-# #   # Sorting by title based on alphabetical order.
-# #   let a_upper = a.title.toUpperCase();
-# #   let b_upper = b.title.toUpperCase();
+# pprint(neuro.serialize())
+# pprint(neuro_copy.serialize())
 
-# #   if (a_upper < b_upper) {
-# #     return -1;
-# #   } else if (a_upper > b_upper) {
-# #     return 1
-# #   } else {
-# #     return 0;
-# #   }
-# # });
+# neuro.year_published = 2022
 
-# # console.log(books);
-# # console.log(booksInLibrary);
+# print("Equality:", neuro_copy == neuro)
+# print("Identity:", neuro_copy is neuro)
 
-# # # Dense data structure
-# # let grid = [
-# #   [1,0,2],
-# #   [0,1,2],
-# #   [0,0,1]
-# # ]
+# pprint(neuro.serialize())
+# pprint(neuro_copy.serialize())
 
-# # # Sparse data structure
-# # let boardState = {
-# #   x_moves: [0,4,8],
-# #   o_moves: [2,5]
-# # }
+# print("\n\nI've added isbns to Neuromacer and Altered Carbon.\n\n")
 
-# # Push: Adds items to an array
-# # console.log(books);
-# # books.push({
-# #   title: "Cryptonomicon",
-# #   author: "Neal Stephenson",
-# #   year_published: 1999,
-# #   isbn: "978-0-380-78862-0",
-# #   rating: 100,
-# #   is_checked_out: false,
-# # });
-# # console.log(books);
+# books_obj[0].isbn = "9780441569595"
+# books_obj[2].isbn = "9780345457684"
 
-# # Pop: removes and item from an array.
-# # console.log(books);
-# # console.log(books.pop());
-# # console.log(books);
+# for book in books_obj:
+#     pprint(book.serialize())
+
+
